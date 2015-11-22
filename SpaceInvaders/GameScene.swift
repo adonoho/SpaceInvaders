3d5690e827aac35ecb9e2dddadc85c80a342b4e5
Robert Murray <work@robertmurray.me>
2015-11-22 12:35:57 -0600

    added invaders

--- SpaceInvaders/GameScene.swift
+++ SpaceInvaders/GameScene.swift
@@ -8,15 +8,105 @@
 
 import SpriteKit
 
+// 1
+enum InvaderType {
+    case A
+    case B
+    case C
+}
+
+//2
+let kInvaderSize = CGSize(width:24, height:16)
+
+
+
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+    
+    let kInvaderGridSpacing = CGSize(width:12, height:12)
+    let kInvaderRowCount = 6
+    let kInvaderColCount = 6
+    let kInvaderName = "invader"
+    
+    var contentCreated: Bool = false
+    
+    func  createContent() -> Bool {
         
-        self.addChild(myLabel)
+//        let invader = SKSpriteNode(imageNamed: "InvaderA_00.png")
+//        invader.position = CGPoint(x: size.width/2, y: size.height/2)
+//        addChild(invader)
+        setupInvaders()
+        
+        backgroundColor = SKColor.blackColor()
+        
+        return true
+    }
+    
+    func makeInvaderOfType( invaderType:InvaderType) -> (SKNode) {
+        var invaderColor: SKColor
+        
+        switch(invaderType) {
+        case .A:
+            invaderColor =  SKColor.redColor()
+        case .B:
+            invaderColor = SKColor.greenColor()
+        case .C:
+            invaderColor = SKColor.blueColor()
+        }
+        
+        let invader = SKSpriteNode(color: invaderColor, size: kInvaderSize)
+        invader.name = kInvaderName
+        
+        return invader
+    }
+    
+    func setupInvaders() {
+        let baseOrigin = CGPoint(x: size.width/3, y: 180)
+        
+        for row in 1...kInvaderRowCount {
+            var invaderType : InvaderType
+            
+            switch row % 3 {
+            case 0:
+                invaderType = .A
+            case 1:
+                invaderType = .B
+            case 2:
+                invaderType = .C
+            default:
+                invaderType = .A
+            }
+            
+            let invaderPositionY = CGFloat(row) * (kInvaderSize.height * 2) + baseOrigin.y
+            var invaderPosition = CGPoint(x: baseOrigin.x, y: invaderPositionY)
+            
+            for _ in 1...kInvaderColCount {
+                let invader = makeInvaderOfType(invaderType)
+                invader.position = invaderPosition
+                addChild(invader)
+                
+                invaderPosition = CGPoint(
+                    x: invaderPosition.x + kInvaderSize.width + kInvaderGridSpacing.width,
+                    y: invaderPositionY)
+                
+            }
+        }
+    }
+    
+    
+    override func didMoveToView(view: SKView) {
+        
+        if !contentCreated {
+            contentCreated = createContent()
+        }
+        /* Setup your scene here */
+//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+//        myLabel.text = "Hello, World!";
+//        myLabel.fontSize = 65;
+//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        
+//        self.addChild(myLabel)
+        
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
