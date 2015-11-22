7f51f1eb7b2651ce8728e81a326508f53a5d193d
Andrew W. Donoho <github@store.ddg.com>
2015-11-22 11:38:53 -0600

    Assets from Wenderlich Tutorial.

--- SpaceInvaders/AppDelegate.swift
+++ SpaceInvaders/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+let log = XCGLogger.defaultInstance()
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        log.debug("Starting up.")
+
         return true
     }
 
